e04705015881fd96977700a514b91ddf8b8eba8d
Patrick Poetz <kellervater@gmail.com>
2024-09-20 16:13:13 +0200

    chore: settings tweaks

--- security/yubi/unix/setup_yubi.sh
+++ security/yubi/unix/setup_yubi.sh
@@ -10,21 +10,29 @@ sudo apt update && sudo apt upgrade -y && \
 
 # Path Magic
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+# yubirc
 # to not unnecessarily pollute the .bashrc file we'll only reference another rc
 mkdir -p ~/.bashrc.d
 cp "${SCRIPT_DIR}/.yubirc" ~/.bashrc.d/
 YUBI_LINE=". ~/.bashrc.d/.yubirc"
 grep -qF "${YUBI_LINE}" ~/.bashrc || sh -c "echo '${YUBI_LINE}' >> ~/.bashrc"
 
-# Verify subscript
+# Verify .bashrc subscript
 echo
 . ~/.bashrc.d/.yubirc
 ssh-add -L
 echo "ℹ️ NOTE: You may need to restart another shell for this to take effect!"
 
-# TODO: it probably makes sense to do a gh auth login or similar
-read -p "Enter Github username: " GH_USER
-gpg --import <(curl -s https://github.com/${GH_USER}.gpg)
+# gpg-agent config
+cp "${SCRIPT_DIR}/gpg-agent.conf" ~/.gnupg/gpg-agent.conf
+gpg-connect-agent killagent /bye
+gpg-connect-agent /bye
+
+# Test Signing
+# NOTE: We do assume a github user here.
+GPG_USERNAME="$(gpg --card-status | grep 'Login data' | awk -F ': ' '{print $2}')"
+gpg --import <(curl -s https://github.com/${GPG_USERNAME}.gpg)
 echo "something" > file.txt
 gpg --sign file.txt
 rm -rf file.txt*
@@ -38,3 +46,4 @@ git config --global user.name "$CARDHOLDER_NAME"
 git config --global user.email $CARDHOLDER_EMAIL
 git config --global commit.gpgSign true
 git config --global user.signingkey $GPG_KEY_ID
+git config --global pull.rebase true
