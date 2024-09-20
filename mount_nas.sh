fcfb8b6b4b5226d1f98be1db781e8eb336b9d1a3
Patrick Pötz <33324812+kellervater@users.noreply.github.com>
2024-09-20 11:27:31 +0200

    fix: exit on error. mount during script execution

--- mount_nas.sh
+++ mount_nas.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eo pipefail
+
 NAS_IP=192.168.0.115
 NAS_SHARE=kellerfilme
 NAS_ADDRESS=//$NAS_IP/$NAS_SHARE
@@ -10,10 +12,15 @@ DEFAULT_NAS_USER=kellervater
 #read -p "Enter NAS username (default: $DEFAULT_NAS_USER): " NAS_USER
 NAS_USER=${NAS_USER:-$DEFAULT_NAS_USER}
 read -sp "Enter password for ${NAS_USER}@${NAS_ADDRESS}: " PASSWORD
+echo "capture password (testing): $PASSWORD"
 
+# TODO - check access before writing to fstab to make it more resilient
 FSTAB_LINE="${NAS_ADDRESS} ${MOUNT_POINT} cifs username=${NAS_USER},password=${PASSWORD},uid=${USER},gid=${USER},file_mode=0777,dir_mode=0777 0 0"
+echo "fstab line: $FSTAB_LINE"
 grep -qF "${NAS_ADDRESS} ${MOUNT_POINT}" /etc/fstab || sudo sh -c "echo '${FSTAB_LINE}' >> /etc/fstab"
 
+sudo systemctl daemon-reload
+sudo mount -a
 
 # Create symlink (workspace dir pointing to repositories on NAS)
 TARGET="/mnt/nas/Job/repos/"
