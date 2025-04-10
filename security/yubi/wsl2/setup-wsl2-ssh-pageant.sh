c502724f3131d305ea966342347d0eeabce939ef
Patrick Pötz <33324812+kellervater@users.noreply.github.com>
2025-04-10 19:12:06 +0200

    Update setup-wsl2-ssh-pageant.sh

--- security/yubi/wsl2/setup-wsl2-ssh-pageant.sh
+++ security/yubi/wsl2/setup-wsl2-ssh-pageant.sh
@@ -2,6 +2,7 @@
 
 # TODO: fork the repository or integrate contents here to ensure availability
 windows_destination="/mnt/c/Users/Public/Downloads/wsl2-ssh-pageant.exe"
+mkdir -p "$HOME/.ssh"
 linux_destination="$HOME/.ssh/wsl2-ssh-pageant.exe"
 wget -O "$windows_destination" "https://github.com/BlackReloaded/wsl2-ssh-pageant/releases/latest/download/wsl2-ssh-pageant.exe"
 # Set the executable bit.
